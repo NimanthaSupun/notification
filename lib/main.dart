e87788241060e1ea8fbde84d55c91209a2a95c60
Nimantha Supun <supunnimantha199@gmail.com>
2024-11-27 03:44:58 +0530

    initialized push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:notify/firebase_options.dart';
 import 'package:notify/notifications/local_notifications.dart';
+import 'package:notify/notifications/push_notifications.dart';
 import 'package:notify/screens/home_page.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
@@ -12,6 +16,49 @@ void main() async {
 
   // initialized timezone
   tz.initializeTimeZones();
+
+  // firebase initialize
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+    //initialize the push notification service (PushNotificationsService)
+  await PushNotificationsService.init();
+
+  //listen for incoming messages in background
+  FirebaseMessaging.onBackgroundMessage(
+      PushNotificationsService.onBackgroundMessage);
+
+  // on background notification tapped
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+    if (message.notification != null) {
+      print("Background Notification Tapped");
+      await PushNotificationsService.onBackgroundNotificationTapped(
+        message,
+        navigatorKey,
+      );
+    }
+  });
+
+  // on foreground notification tapped
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+  //   await PushNotificationsService.onForeroundNotificationTapped(
+  //     message,
+  //     navigatorKey,
+  //   );
+  // });
+
+  // for handling in terminated state
+  final RemoteMessage? message =
+      await FirebaseMessaging.instance.getInitialMessage();
+
+  if (message != null) {
+    print("Launched from terminated state");
+    Future.delayed(const Duration(seconds: 1), () {
+      navigatorKey.currentState!.pushNamed("/message", arguments: message);
+    });
+  }
+
 
   runApp(const MyApp());
 }
